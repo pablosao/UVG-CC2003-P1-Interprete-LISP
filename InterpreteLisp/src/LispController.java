0cd7a8b85a1094cff7acae33b50eb1d0abc97946
Pablo Sao <pablo_sao@outlook.com>
2019-03-13 09:35:11 -0600

    Se comenta partes pendientes para evitar problemas de ejecución

--- InterpreteLisp/src/LispController.java
+++ InterpreteLisp/src/LispController.java
@@ -37,45 +37,37 @@ public class LispController implements iLispController{
 
     @Override
     public boolean verificaSinbolos(String instruccion) {
-        boolean isOK = false;
-        DataManager archivo = new DataManager();
-        
-        
-        for(int control = 0;control<SIMBOLOS_PERMITIDOS.length;control++){
-            isOK = archivo.contieneChar(instruccion, SIMBOLOS_PERMITIDOS[control]);
-        }
-        
-        return isOK;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 
     @Override
     public List getInstruccion(List instruccion) {
         List seccion = new ArrayList();
-        
-        String registro = instruccion.get(0).toString();
-                
-        if(registro.equals("(") ){
-            instruccion.remove(0);
-            while(!registro.equals(")")){
-                try{
-                    registro = instruccion.get(0).toString();
-                    seccion.add(Integer.parseInt(registro));
-                }
-                catch(NumberFormatException e){
-                    seccion.add(registro);
-                }
-                catch(Exception e){}
-                finally{
-                    instruccion.remove(0);
-                    getInstruccion(instruccion);
-                }
-            }
-        }
-        else if (registro.equals(")")){
-            //
-            throw new UnsupportedOperationException("Se encontro un operador no valido.");
-        }
+//        
+//        String registro = instruccion.get(0).toString();
+//                
+//        if(registro.equals("(") ){
+//            instruccion.remove(0);
+//            while(!registro.equals(")")){
+//                try{
+//                    registro = instruccion.get(0).toString();
+//                    seccion.add(Integer.parseInt(registro));
+//                }
+//                catch(NumberFormatException e){
+//                    seccion.add(registro);
+//                }
+//                catch(Exception e){}
+//                finally{
+//                    instruccion.remove(0);
+//                    getInstruccion(instruccion);
+//                }
+//            }
+//        }
+//        else if (registro.equals(")")){
+//            //
+//            throw new UnsupportedOperationException("Se encontro un operador no valido.");
+//        }
         
         return seccion;
     }
