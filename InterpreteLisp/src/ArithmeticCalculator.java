00be082ebf8945e8bad10b8c44f264bc4ae1f177
Pablo Sao <pablo_sao@outlook.com>
2019-03-15 09:27:31 -0600

    Componiendo para que sume más de 2 números

--- InterpreteLisp/src/ArithmeticCalculator.java
+++ InterpreteLisp/src/ArithmeticCalculator.java
@@ -15,24 +15,42 @@ public class ArithmeticCalculator {
     public Double calculate(List<Object> prefixList){
 
         String value = "";
+        
+        String signo = String.valueOf(prefixList.get(0));
+        
 
-        for (int i = 0; i < prefixList.size(); i++){
-            if (prefixList.get(i) instanceof String){
-                if (String.valueOf(prefixList.get(i)).matches("[+]")){//Suma
-                    if (prefixList.get(i + 1) instanceof Integer){
-                        value = "" + prefixList.get(i + 1);
+        for (int i = 1; i < prefixList.size(); i++){
+            //if (prefixList.get(i) instanceof String){
+                //if (String.valueOf(prefixList.get(i)).matches("[+]")){//Suma
+                if (signo.matches("[+]")){//Suma
+                    
+                    if(prefixList.get(i) instanceof Integer || prefixList.get(i) instanceof Double ){
+                        value = "" + prefixList.get(i); 
                         result.push(Double.parseDouble(value));
-                        if (prefixList.get(i + 2) instanceof ArrayList){
-                            calculate((List)prefixList.get(i + 2));
-                        } else if (prefixList.get(i + 2) instanceof Integer){
-                            value = "" + prefixList.get(i + 2);
-                            result.push(Double.parseDouble(value));
-                        }
                     }
-                    result.push(sumar());
+                    
+                    else if(prefixList.get(i) instanceof ArrayList){
+                        calculate((List)prefixList.get(i));
+                    }
+                    
+                    
+//                    if (prefixList.get(i + 1) instanceof Integer){
+//                        value = "" + prefixList.get(i + 1); 
+//                        result.push(Double.parseDouble(value));
+//                        
+//                        if (prefixList.get(i + 2) instanceof ArrayList){ 
+//                            calculate((List)prefixList.get(i + 2));
+//                        } else if (prefixList.get(i + 2) instanceof Integer){
+//                            value = "" + prefixList.get(i + 2);
+//                            result.push(Double.parseDouble(value));
+//                        }
+//                    }
+//                    result.push(sumar());
+                    
                 }
 
-                if (String.valueOf(prefixList.get(i)).matches("[-]")){//Resta
+                //if (String.valueOf(prefixList.get(i)).matches("[-]")){//Resta
+                if (signo.matches("[-]")){//Resta
                     if (prefixList.get(i + 1) instanceof Integer){
                         value = "" + prefixList.get(i + 1);
                         result.push(Double.parseDouble(value));
@@ -43,10 +61,13 @@ public class ArithmeticCalculator {
                             result.push(Double.parseDouble(value));
                         }
                     }
+                    
                     result.push(restar());
+                    
                 }
 
-                if (String.valueOf(prefixList.get(i)).matches("[*]")){//Multiplicar
+                //if (String.valueOf(prefixList.get(i)).matches("[*]")){//Multiplicar
+                if (signo.matches("[*]")){//Multiplicar
                     if (prefixList.get(i + 1) instanceof Integer){
                         value = "" + prefixList.get(i + 1);
                         result.push(Double.parseDouble(value));
@@ -55,12 +76,14 @@ public class ArithmeticCalculator {
                         } else if (prefixList.get(i + 2) instanceof Integer){
                             value = "" + prefixList.get(i + 2);
                             result.push(Double.parseDouble(value));
+                            
                         }
                     }
                     result.push(multiplicar());
                 }
 
-                if (String.valueOf(prefixList.get(i)).matches("[/]")){//Dividir
+                //if (String.valueOf(prefixList.get(i)).matches("[/]")){//Dividir
+                if (signo.matches("[/]")){//Dividir
                     if (prefixList.get(i + 1) instanceof Integer){
                         value = "" + prefixList.get(i + 1);
                         result.push(Double.parseDouble(value));
@@ -74,17 +97,29 @@ public class ArithmeticCalculator {
                     result.push(dividir());
                 }
 
-                i += 2;
-            }
+                //i += 2;
+            //}
+       
         }
-
+        
+        if(signo.matches("[+]"))
+                result.push(sumar());
+        
         return this.result.peek();
     }
 
     public double sumar(){
-        double a = result.pop();
-        double b = result.pop();
-        return a + b;
+        
+        double res = 0.00;
+        int lenstack = result.size();
+        for(int control = 0; control<lenstack;control++){
+            res += result.pop();
+        }
+//        double a = result.pop();
+//        double b = result.pop();
+        //return a + b;
+        
+        return res;
     }
 
     public double restar(){
