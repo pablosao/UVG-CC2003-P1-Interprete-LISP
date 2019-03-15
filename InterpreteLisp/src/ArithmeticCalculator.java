fdc5d09240d33d373d2dee4962c64d617e4d8427
Pablo Sao <pablo_sao@outlook.com>
2019-03-15 11:31:24 -0600

    Corrigiendo para operación aritmetica recursiva por medio de stack

--- InterpreteLisp/src/ArithmeticCalculator.java
+++ InterpreteLisp/src/ArithmeticCalculator.java
@@ -10,134 +10,94 @@ import java.util.Stack;
  */
 public class ArithmeticCalculator {
 
-    private Stack<Double> result = new Stack<>();
-
     public Double calculate(List<Object> prefixList){
-
+        
+        Stack<Double> result = new Stack<>();
+        
         String value = "";
         
         String signo = String.valueOf(prefixList.get(0));
         
 
         for (int i = 1; i < prefixList.size(); i++){
-            //if (prefixList.get(i) instanceof String){
-                //if (String.valueOf(prefixList.get(i)).matches("[+]")){//Suma
-                if (signo.matches("[+]")){//Suma
-                    
-                    if(prefixList.get(i) instanceof Integer || prefixList.get(i) instanceof Double ){
-                        value = "" + prefixList.get(i); 
-                        result.push(Double.parseDouble(value));
+            if(prefixList.get(i) instanceof Integer || prefixList.get(i) instanceof Double ){
+                value = "" + prefixList.get(i); 
+                result.push(Double.parseDouble(value));
                     }
                     
                     else if(prefixList.get(i) instanceof ArrayList){
-                        calculate((List)prefixList.get(i));
+                        result.push(calculate((List)prefixList.get(i)));
                     }
-                    
-                    
-//                    if (prefixList.get(i + 1) instanceof Integer){
-//                        value = "" + prefixList.get(i + 1); 
-//                        result.push(Double.parseDouble(value));
-//                        
-//                        if (prefixList.get(i + 2) instanceof ArrayList){ 
-//                            calculate((List)prefixList.get(i + 2));
-//                        } else if (prefixList.get(i + 2) instanceof Integer){
-//                            value = "" + prefixList.get(i + 2);
-//                            result.push(Double.parseDouble(value));
-//                        }
-//                    }
-//                    result.push(sumar());
-                    
-                }
-
-                //if (String.valueOf(prefixList.get(i)).matches("[-]")){//Resta
-                if (signo.matches("[-]")){//Resta
-                    if (prefixList.get(i + 1) instanceof Integer){
-                        value = "" + prefixList.get(i + 1);
-                        result.push(Double.parseDouble(value));
-                        if (prefixList.get(i + 2) instanceof ArrayList){
-                            calculate((List)prefixList.get(i + 2));
-                        } else if (prefixList.get(i + 2) instanceof Integer){
-                            value = "" + prefixList.get(i + 2);
-                            result.push(Double.parseDouble(value));
-                        }
-                    }
-                    
-                    result.push(restar());
-                    
-                }
-
-                //if (String.valueOf(prefixList.get(i)).matches("[*]")){//Multiplicar
-                if (signo.matches("[*]")){//Multiplicar
-                    if (prefixList.get(i + 1) instanceof Integer){
-                        value = "" + prefixList.get(i + 1);
-                        result.push(Double.parseDouble(value));
-                        if (prefixList.get(i + 2) instanceof ArrayList){
-                            calculate((List)prefixList.get(i + 2));
-                        } else if (prefixList.get(i + 2) instanceof Integer){
-                            value = "" + prefixList.get(i + 2);
-                            result.push(Double.parseDouble(value));
-                            
-                        }
-                    }
-                    result.push(multiplicar());
-                }
-
-                //if (String.valueOf(prefixList.get(i)).matches("[/]")){//Dividir
-                if (signo.matches("[/]")){//Dividir
-                    if (prefixList.get(i + 1) instanceof Integer){
-                        value = "" + prefixList.get(i + 1);
-                        result.push(Double.parseDouble(value));
-                        if (prefixList.get(i + 2) instanceof ArrayList){
-                            calculate((List)prefixList.get(i + 2));
-                        } else if (prefixList.get(i + 2) instanceof Integer){
-                            value = "" + prefixList.get(i + 2);
-                            result.push(Double.parseDouble(value));
-                        }
-                    }
-                    result.push(dividir());
-                }
-
-                //i += 2;
-            //}
-       
         }
         
-        if(signo.matches("[+]"))
-                result.push(sumar());
+        if(signo.matches("[+]")){
+            result.push(sumar(result));
+        }
+        if(signo.matches("[-]")){
+            result.push(restar(result));
+        }
+        if(signo.matches("[*]")){
+            result.push(multiplicar(result));
+        }
+        if(signo.matches("[/]")){
+            result.push(dividir(result));
+        }
         
-        return this.result.peek();
+        return result.peek();
     }
 
-    public double sumar(){
+    public double sumar(Stack<Double> value){
         
         double res = 0.00;
-        int lenstack = result.size();
+        int lenstack = value.size();
         for(int control = 0; control<lenstack;control++){
-            res += result.pop();
+            res += value.pop();
         }
-//        double a = result.pop();
-//        double b = result.pop();
-        //return a + b;
-        
         return res;
     }
 
-    public double restar(){
-        double a = result.pop();
-        double b = result.pop();
-        return b - a;
+    public double restar(Stack<Double> value){
+        
+        Stack<Double> temp_stack = revertStack(value);
+        
+        double res = temp_stack.pop();
+        int lenstack = temp_stack.size();
+        
+        for(int control = 0; control<lenstack;control++){
+            res -= temp_stack.pop();
+        }
+        return res;
     }
 
-    public double multiplicar(){
-        double a = result.pop();
-        double b = result.pop();
-        return a * b;
+    public double multiplicar(Stack<Double> value){
+        double res = value.pop();
+        int lenstack = value.size();
+        for(int control = 0; control<lenstack;control++){
+            res *= value.pop();
+        }        
+        return res;
     }
 
-    public double dividir(){
-        double a = result.pop();
-        double b = result.pop();
-        return (b/a);
+    public double dividir(Stack<Double> value){
+        
+        Stack<Double> temp_stack = revertStack(value);
+        
+        double res = temp_stack.pop();
+        int lenstack = temp_stack.size();
+        
+        for(int control = 0; control<lenstack;control++){
+            res /= temp_stack.pop();
+        }
+        
+        return res;
     }
-
+    
+    public Stack<Double> revertStack(Stack<Double> value){
+        Stack<Double> temp_stack = new Stack();
+        while(!value.empty()){
+            temp_stack.add((double)value.pop());
+        }
+        
+        return temp_stack;
+    } 
 }
