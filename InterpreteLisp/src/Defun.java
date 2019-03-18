55edaf398fbadafa379b6f21f7e4d2e75245aaa6
Juan De Leon <dele17822@uvg.edu.gt>
2019-03-17 19:55:20 -0600

    Comentarios

--- InterpreteLisp/src/Defun.java
+++ InterpreteLisp/src/Defun.java
@@ -8,6 +8,12 @@ public class Defun {
     private HashMap<String, Object> variables = new HashMap<>();
     private List<Object> instructions;
 
+    /**
+     * Constructor de Defun
+     * @param funName Indica el nombre de la funcion
+     * @param vars Mapa con las variables de la funcion
+     * @param inst Instrucciones de la funcion
+     */
     public Defun(String funName, Object vars, Object inst){
         List<String> variables = (List)vars;
         List instructions = (List) inst;
@@ -18,23 +24,27 @@ public class Defun {
         this.instructions = instructions;
     }
 
+    /**
+     * Metodo que ejecuta la funcion
+     * @param variables lista con las variables de la funcion
+     * @return Devuelve la instruccion con las variables modificadas como constantes
+     */
     public List executeInstructions(List variables){
         List tempInstructions = this.instructions;
-        HashMap<String, Object> tempVariables = this.variables;
+        HashMap<String, Object> tempVariables = this.variables;//Mapa temporal para almacenar el valor constante de cada variable
         //System.out.println(tempInstructions);
         if (variables.size() == tempVariables.size()){//verifica si la lista es del tamaño necesario para ejecutar la funcion
-            System.out.println("Entra");
             int i = 0;
-            for (String key: tempVariables.keySet()) {
+            for (String key: tempVariables.keySet()) {//Toma cada elemento del map y le asigna un Value
                 tempVariables.replace(key, variables.get(i));
                 i++;
             }
             //System.out.println(tempInstructions);
             i = 0;
             while (i < tempInstructions.size()){
-                if ((tempInstructions.get(i) instanceof String)){
-                    for (String key: tempVariables.keySet()) {
-                        if (tempInstructions.get(i).equals(key)){
+                if ((tempInstructions.get(i) instanceof String)){//Toma cada elemento de las instrucciones
+                    for (String key: tempVariables.keySet()) {//Toma cada elemento del map
+                        if (tempInstructions.get(i).equals(key)){//Si el elemento de la instruccion es igual a algun Key del mapa lo reemplaza por su Value
                             tempInstructions.add(i, tempVariables.get(key));
                             tempInstructions.remove(i + 1);
                         }
